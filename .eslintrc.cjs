e5f55f4a7bfb312132eb017a0e7dc89b13040ee6
Mikko <jokipuum@gmail.com>
2023-01-13 11:39:48 +0200

    Setting up ESLint, Prettier, and TypeScript

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ["plugin:astro/recommended"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "astro/no-set-html-directive": "error",
+        "astro/no-set-text-directive": "error",
+        "astro/no-unused-css-selector": "error",
+        semi: "off", // Don't need ESLint's semi
+        "astro/semi": [
+          "error",
+          "always", // or "never"
+          { omitLastInOneLineBlock: true },
+          // or { "beforeStatementContinuationChars": "any" | "always" | "never" }
+        ],
+      },
+    },
+  ],
+};
