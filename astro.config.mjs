f8598831f54b7f9fc76a3c95381af02c0a97d05b
Mikko <jokipuum@gmail.com>
2023-01-15 15:21:01 +0200

    Adding @astrojs/image, sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [
+    tailwind(),
+    react(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
 });
