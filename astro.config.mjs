a5044b78da41cf4b5a5facd1dcc72c7962cc3121
Mikko <jokipuum@gmail.com>
2023-01-18 12:58:35 +0200

    Adding @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://jokipuu.dev",
   integrations: [
     tailwind(),
     react(),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
   ],
 });
